fe30d0090d10d69e701bd8dbcb07559e7a96add6
Nathan Bird <nathan@acceleration.net>
2008-06-13 13:55:45 -0400

    some work on the socket-messenger, getting that moving a bit
    darcs-hash:20080613175545-783f4-eb61df9c9b52a87b1e2a566e053ecee0e389dbf8.gz

---
+++ src/socket-messenger.lisp
@@ -0,0 +1,97 @@
+(in-package :cl-monetra)
+
+(defconstant +STX+ #x02)
+(defconstant +FS+ #x1c)
+(defconstant +ETX+ #x03)
+(defparameter +monetra-enc+ (flex:make-external-format :utf-8))
+
+(defun write-message (sslstream key message)
+  (unless (open-stream-p sslstream)
+    (error "Attempting to write message to closed stream. ~a" sslstream))
+  (flet ((ws (s)
+	   (write-sequence (etypecase s
+			     (string (flex:string-to-octets s :external-format +monetra-enc+))
+			     (flex:octet s))
+			   sslstream))
+	 (wb (b) (write-byte b sslstream)))
+    (wb +STX+)
+    (ws key)
+    (wb +FS+)
+    (ws message)
+    (wb +ETX+))
+  (finish-output sslstream))
+
+(defun message-from-stream (sslstream)
+  (unless (and (input-stream-p sslstream)
+	       (subtypep (stream-element-type ssltream) 'flexi-streams:octet)
+	       (open-stream-p sslstream))
+    (error "Stream must be an open binary input stream. ~a" sslstream))
+  ;;make sure we have a start tx byte.
+  (let ((b (read-byte sslstream)))
+    (unless (= +STX+ b)
+      (error "Expected STX(~a) on stream, found ~a" +STX+ b)))
+  ;;read the message id
+  (let ((idbuf (make-array 8 :element-type 'flexi-streams:octet :adjustable t :fill-pointer 0 ))
+	(keybuf (make-array 16 :element-type 'flexi-streams:octet :adjustable t :fill-pointer 0 ))
+	(valbuf (make-array 16 :element-type 'flexi-streams:octet :adjustable t :fill-pointer 0 ))
+	(msghash (make-hash-table :test 'eq))
+	(buf))			       ;pointer to which one is active
+    (setf buf keybuf)			;start pointing at the key
+    (flet ((finish-line ()
+	     (let ((*package* (find-package :keyword)) ;want to read in as a keyword
+		   (*read-eval* nil))	;just want to be safe(r)
+	       (let ((k (read-from-string (octets-to-string keybuf +monetra-enc+)))
+		     (v (octets-to-string valbuf +monetra-enc+)))
+		 (setf (gethash k msghash) v
+		       (fill-pointer keybuf) 0 ;reset the buffers.
+		       (fill-pointer valbuf) 0
+		       buf keybuf     ;reset the active buffer pointer
+		       )))))
+      ;; Gathering up the ID is pretty direct, just read till #\FS
+      (loop for b = (read-byte sslstream)
+	 until (= b +FS+)
+	 do (vector-push-extend b idbuf))
+      
+      (loop
+	 with buf = keybuf
+	 for b = (read-byte sslstream)
+	 do (case b
+	      (+ETX+)
+	      (13 (finish-line))   ;end of line, switch back to keybuf
+	      (10)				  ;ignore
+	      (#.(char-code #\=) (setf buf valbuf)) ; = indicates start of value
+	      (t (vector-push-extend b buf))) ;otherwise(data) add to active buf
+	 ))
+    
+    (values msghash (octets-to-string idbuf +monetra-enc+))))
+
+(defun test ()
+  (let* ((usocket (usocket:socket-connect "transact.merchantprovider.com" 8444
+					  :element-type 'flexi-streams:octet))
+	 (socket (usocket:socket usocket))
+;;	 (f (setf (sb-bsd-sockets:non-blocking-mode socket) t))
+	 (sockstream (usocket:socket-stream usocket))
+	 (fd (cl+ssl:stream-fd sockstream))
+	 (sslstream (cl+ssl:make-ssl-client-stream
+		     fd :close-callback (lambda ()
+					  (break "closing sockstream: ~a" sockstream)
+					  (close sockstream))))
+	 (fstream (flexi-streams:make-flexi-stream sslstream
+						   :external-format (flex:make-external-format :utf-8)))
+	 (msgbody "username=adwtest
+password=testtest
+action=chkpwd
+"))
+    
+    (write-message sslstream "asf324f" msgbody)
+    
+    (let (
+	  (buf (make-array 1024 :fill-pointer 0 :element-type 'octet ))
+	  ;;(buf (make-array 128 :element-type 'flexi-streams:octet ))
+	  )
+      (loop for c = (read-byte sslstream)
+	 until (= c +etx+)
+	 do
+	   (vector-push c buf) )
+      
+      buf)))

38a51b94c9525daddc7ebbc43aa29f91440a4811
Nathan Bird <nathan@acceleration.net>
2008-06-13 13:54:55 -0400

    woefully incomplete start at the monetra-processor file
    darcs-hash:20080613175455-783f4-8e14e9b8b107e2102dcfd27177bc50f0b724e1fb.gz

---
+++ src/monetra-processor.lisp
@@ -0,0 +1,14 @@
+(in-package :cl-monetra)
+
+(defclass monetra-processor ()
+  ((host)
+   (port)
+   (conn)
+   (user)
+   (pass)))
+
+(defmethod sale ((mp monetra-processor) cc-data amount &key bill-address &allow-other-keys))
+
+(defmethod authorize ((mp monetra-processor) cc-data amount &key bill-address &allow-other-keys))
+
+(defmethod capture ((mp monetra-processor) transaction-id &key amount  ))
